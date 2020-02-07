96db23ea637c83c289a5d41246b29c42237974f8
Darryl Blackport <darryl@Darryls-MacBook-Pro.local>
2020-02-07 13:37:14 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='nba_stats_tracking',
+    version='0.0.1',
+    author='Darryl Blackport',
+    author_email='darryl.blackport@gmail.com',
+    description='A package to work with NBA player tracking stats using the NBA Stats API',
+    license='MIT License',
+    keywords=['basketball', 'NBA', 'player tracking'],
+    url='https://github.com/dblackrun/nba-stats-tracking',
+    packages=['nba_stats_tracking'],
+    install_requires=['requests', 'python-dateutil'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
