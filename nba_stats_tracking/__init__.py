77b5550aed5bc711038853f03f8fd87eb98769b6
dblackrun <darryl.blackport@gmail.com>
2021-08-28 18:56:33 -0400

    update headers

--- nba_stats_tracking/__init__.py
+++ nba_stats_tracking/__init__.py
@@ -5,12 +5,13 @@ A package to work with NBA player tracking stats from
 `NBA Advanced Stats <https://www.stats.nba.com/>`_.
 """
 
-REQUEST_TIMEOUT = 10
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
-REFERER = "http://stats.nba.com/"
+REQUEST_TIMEOUT = 30
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"
+REFERER = "https://www.nba.com/stats/"
 HEADERS = {
     "User-Agent": USER_AGENT,
     "Referer": REFERER,
+    "Origin": "https://www.nba.com",
     "Accept": "application/json, text/plain, */*",
     "Accept-Language": "en-US,en;q=0.5",
     "x-nba-stats-origin": "stats",
@@ -19,6 +20,7 @@ HEADERS = {
     "Connection": "keep-alive",
     "Pragma": "no-cache",
     "Cache-Control": "no-cache",
+    "TE": "Trailers",
 }
 
 CATCH_SHOOT_MEASURE_TYPE = "CatchShoot"
